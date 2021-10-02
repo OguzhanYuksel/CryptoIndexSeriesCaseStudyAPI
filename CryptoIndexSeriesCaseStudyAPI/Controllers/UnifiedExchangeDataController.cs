ec2d4d20902abaa836d957c17c51797a3c40a31d
OguzhanYuksel <yukseloguzhan4@gmail.com>
2021-10-02 19:59:07 +0300

    Add project files.

---
+++ CryptoIndexSeriesCaseStudyAPI/Controllers/UnifiedExchangeDataController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CryptoIndexSeriesCaseStudyAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UnifiedExchangeDataController : ControllerBase
+    {
+
+        private readonly ILogger<UnifiedExchangeDataController> _logger;
+
+        public UnifiedExchangeDataController(ILogger<UnifiedExchangeDataController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+
+            return Ok();
+        }
+    }
+}
